e49a81a68dc9a7d1269cffbcc832762a70ddea48
Iggy988 <igor.rodic1988@gmail.com>
2024-03-23 09:21:18 +0100

    Events

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -14,5 +14,15 @@ public partial class MainWindow : Window
         resultLabel.Content = "1234";
     }
 
-    
+    private void sevenButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (resultLabel.Content.ToString() == "0")
+        {
+            resultLabel.Content = "7";
+        }
+        else
+        {
+            resultLabel.Content = $"{resultLabel.Content}7";
+        }
+    }
 }
