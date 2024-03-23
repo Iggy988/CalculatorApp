e46522a1d091350d841c59917fc354811248c124
Iggy988 <igor.rodic1988@gmail.com>
2024-03-23 22:38:32 +0100

    Creating EventHandlers

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -7,11 +7,44 @@ namespace Calculator;
 /// </summary>
 public partial class MainWindow : Window
 {
+    double lastNumber, result;
+
     public MainWindow()
     {
         InitializeComponent();
 
-        resultLabel.Content = "1234";
+        acButton.Click += AcButton_Click;
+        minusButton.Click += MinusButton_Click;
+        percentButton.Click += PercentButton_Click;
+        equalsButton.Click += EqualsButton_Click;
+    }
+
+    private void EqualsButton_Click(object sender, RoutedEventArgs e)
+    {
+        
+    }
+
+    private void PercentButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+        {
+            lastNumber = lastNumber / 100;
+            resultLabel.Content = lastNumber.ToString();
+        }
+    }
+
+    private void MinusButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+        {
+            lastNumber = lastNumber * -1;
+            resultLabel.Content = lastNumber.ToString();
+        }
+    }
+
+    private void AcButton_Click(object sender, RoutedEventArgs e)
+    {
+        resultLabel.Content = "0";
     }
 
     private void sevenButton_Click(object sender, RoutedEventArgs e)
