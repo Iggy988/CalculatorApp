f2515b05948f4a92881b76ba28a705e5acddbeae
Iggy988 <igor.rodic1988@gmail.com>
2024-03-24 09:55:27 +0100

    Creating custom types

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -9,20 +9,43 @@ namespace Calculator;
 public partial class MainWindow : Window
 {
     double lastNumber, result;
+    SelectedOperator selectedOperator;
 
     public MainWindow()
     {
         InitializeComponent();
 
         acButton.Click += AcButton_Click;
-        minusButton.Click += MinusButton_Click;
+        //minusButton.Click += MinusButton_Click;
         percentButton.Click += PercentButton_Click;
         equalsButton.Click += EqualsButton_Click;
     }
 
     private void EqualsButton_Click(object sender, RoutedEventArgs e)
     {
-        
+        double newNumber;
+
+        if (double.TryParse(resultLabel.Content.ToString(), out newNumber))
+        {
+            switch (selectedOperator) 
+            {
+                case SelectedOperator.Addition:
+                    result = SimpleMath.Add(lastNumber, newNumber);
+                    break;
+                case SelectedOperator.Subtraction:
+                    result = SimpleMath.Subtract(lastNumber, newNumber);
+                    break;
+                case SelectedOperator.Division:
+                    result = SimpleMath.Divide(lastNumber, newNumber);
+                    break;
+                case SelectedOperator.Multiplication:
+                    result = SimpleMath.Multiply(lastNumber, newNumber);
+                    break;
+
+            }
+
+            resultLabel.Content = result.ToString(); 
+        }
     }
 
     private void PercentButton_Click(object sender, RoutedEventArgs e)
@@ -56,6 +79,28 @@ public partial class MainWindow : Window
         {
             resultLabel.Content = "0";
         }
+
+        if (sender == multiplyButton)
+            selectedOperator = SelectedOperator.Multiplication;
+        if (sender == divideButton)
+            selectedOperator = SelectedOperator.Division;
+        if (sender == plusButton)
+            selectedOperator = SelectedOperator.Addition;
+        if (sender == minus_button)
+            selectedOperator = SelectedOperator.Subtraction;
+    }
+
+    private void dotButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (resultLabel.Content.ToString().Contains("."))
+        { 
+            //do nothing
+        }
+        else
+        {
+            resultLabel.Content = $"{resultLabel.Content}.";
+        }
+        
     }
 
     private void NumberButton_Click(object sender, RoutedEventArgs e)
@@ -70,5 +115,34 @@ public partial class MainWindow : Window
         {
             resultLabel.Content = $"{resultLabel.Content}{selectedValue}";
         }
+    }
+}
+
+
+public enum SelectedOperator
+{
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division,
+}
+
+public class SimpleMath
+{
+    public static double Add(double n1, double n2)
+    {
+        return n1 + n2;
+    }
+    public static double Subtract(double n1, double n2)
+    {
+        return n1 - n2;
+    }
+    public static double Multiply(double n1, double n2)
+    {
+        return n1 * n2;
+    }
+    public static double Divide(double n1, double n2)
+    {
+        return n1 / n2;
     }
 }
